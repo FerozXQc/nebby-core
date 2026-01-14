230c185f0e31218bdcc934e23a433223fe4aa8a7
FerozxQc <ferozchand06@gmail.com>
2026-01-14 21:59:58 +0530

    aws-onboarding url api

--- src/api/app.py
+++ src/api/app.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
-from src.api.routers import auth_router
+from src.api.routers import auth_router, template_router
+
 app = FastAPI()
 app.include_router(auth_router, prefix='/auth',tags=['auth'])
+app.include_router(template_router, prefix='/template',tags=['template'])
+
 @app.get('/')
 def health_check():
     return 'hello world'
